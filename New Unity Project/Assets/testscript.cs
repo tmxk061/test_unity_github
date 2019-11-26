9a3e342c627469f6bef5021bb1b18372e5b0aec3
SEOSEOKGWON <tmxk061@naver.com>
2019-11-26 13:24:03 +0900

    add-테스트
    2번에서 테스트

---
+++ New Unity Project/Assets/testscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("2번에서 생성한 코드다");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
