97509b35bb21f0b879285db709d0af84e0b32f40
SEOSEOKGWON <tmxk061@naver.com>
2019-11-26 15:39:18 +0900

    23
    23232

--- New Unity Project/Assets/testscript.cs
+++ New Unity Project/Assets/testscript.cs
@@ -7,21 +7,7 @@ public class testscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("2번에서 생성한 코드다");
-
-        Debug.Log("박창현");
-
-        Debug.Log("코드바뀌는거 테스트 하는 코드다!!!!!!!!를 2번에서 바꿔 보았다.");
-
-        Debug.Log("리베이스 테스트용 코드를 한번 바꿔보았다.");
-
-        Debug.Log("리베이스 테스트용 코드를 한번 바꿔보았다.");
-
-        Debug.Log("리베이스 테스트용 코드를 한번 바꿔보았다.");
-
-        Debug.Log("리베이스 테스트용 코드를 한번 바꿔보았다.");
-
-        Debug.Log("리베이스 테스트용 코드를 한번 바꿔보았다.");
+        
 
 
     }
